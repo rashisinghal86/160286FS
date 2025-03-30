cc98cebea34faa9d370707a75b63e1cd01db8493
rashisinghal86 <21f1005286@ds.study.iitm.ac.in>
2025-03-30 11:09:28 +0530

    user based  nav bar working with logout

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,34 +2,33 @@ export default {
     props: ['loggedIn'],
     template: `
     <div class="row border">
-        <div class="col-10 fs-2 border">
-            Platform
+        <div class="col-8 fs-2 fw-bold text-center border">
+            Platform Household Services 
         </div>
-        <div class="col-2 border">
+        <div class="col-4 border ">
             <div class="mt-1">
-            <router-link v-if="!$store.state.loggedIn" to='/homecss'>Home</router-link>
-            <router-link v-if="!$store.state.loggedIn" to='/api/login'>Login</router-link>
-            <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
-            <router-link v-if="$store.state.loggedIn" to='/profile'>Profile</router-link>
+            <router-link v-if="!$store.state.loggedIn" to='/homecss'> Home |</router-link>
+            <router-link v-if="!$store.state.loggedIn" to='/api/login'> Login |</router-link>
+            <router-link v-if="!$store.state.loggedIn" to='/register'> Register |</router-link>
+            <router-link v-if="$store.state.loggedIn" to='/profile'> Profile |</router-link>
 
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/api/admin_db'>Home</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/admin_booking'>Overview</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/categories'>Service Management</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/pending_professionals'>Professional Management</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/manage_customers'>Customer Management</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/api/admin_db'> Home |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/admin_booking'> Overview |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/categories'> Services |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/pending_professionals'> Professionals |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/manage_customers'> Customers </router-link>
             
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_db'>Home</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/view_appointments'>Schedules</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_booking'>Bookings</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_db'> Home |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/view_appointments'> Schedules |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_booking'> Bookings |</router-link>
 
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_db'>Home</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/catalogue'>View Services</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/schedule'>Schedules</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_bookings'>Bookings</router-link>
-
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_db'> Home |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/catalogue'>View Services |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/schedule'>Schedules |</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_bookings'>Bookings |</router-link>
 
                 
-                <button v-if="$store.state.loggedIn" @click="logoutUser" class="btn btn-danger">Logout</button>
+                <button v-if="$store.state.loggedIn" @click="logoutUser" class="btn btn-danger ms-auto d-block" >Logout</button>
 
             </div>
         </div>
@@ -41,7 +40,16 @@ export default {
     },
     methods:{
         logoutUser(){
-            localStorage.clear()
+            localStorage.clear();
+            
+            this.$store.commit('setUser', {
+                auth_token: null,
+                role: null,
+                loggedIn: false,
+                user_id: null
+            });
+            
+
             this.$router.push('/home')
             this.$emit('logout')
         }
