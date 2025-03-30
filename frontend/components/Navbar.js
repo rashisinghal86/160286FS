da2d2e4db91606c8ee4312c0e0e83de9e753989e
rashisinghal86 <21f1005286@ds.study.iitm.ac.in>
2025-03-30 10:09:35 +0530

    user based  nav bar working with logout

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,25 +1,50 @@
 export default {
-    template : `
-    <div>
-        <router-link to='/homecss'>Home</router-link>
-        <router-link v-if="!$store.state.loggedIn" to='/api/login'>Login</router-link>
-        <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
+    props: ['loggedIn'],
+    template: `
+    <div class="row border">
+        <div class="col-10 fs-2 border">
+            Platform
+        </div>
+        <div class="col-2 border">
+            <div class="mt-1">
+            <router-link v-if="!$store.state.loggedIn" to='/homecss'>Home</router-link>
+            <router-link v-if="!$store.state.loggedIn" to='/api/login'>Login</router-link>
+            <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
+            <router-link v-if="$store.state.loggedIn" to='/profile'>Profile</router-link>
 
-        <router-link v-if="$store.state.loggedIn" to='/profile'>Profile</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/admin_booking'>Overview</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/add_category'>Service Management</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/pending_professionals'>Professional Management</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/manage_customers'>Customer Management</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/api/admin_db'>Home</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/admin_booking'>Overview</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/categories'>Service Management</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/pending_professionals'>Professional Management</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/manage_customers'>Customer Management</router-link>
+            
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_db'>Home</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/view_appointments'>Schedules</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Professional'" to='/prof_booking'>Bookings</router-link>
+
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_db'>Home</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/catalogue'>View Services</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/schedule'>Schedules</router-link>
+            <router-link v-if="$store.state.loggedIn && $store.state.role == 'Customer'" to='/cust_bookings'>Bookings</router-link>
 
 
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/admin_db'>Admin Dashboard</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'Admin'" to='/'>Admin Dashboard</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'customer'" to='/api/login'>customerlogin</router-link>
-        <router-link v-if="$store.state.loggedIn && $store.state.role == 'professional'" to='/api/login'>profflogin</router-link>
-        #signout
-        <router-link v-if="$store.state.loggedIn" to='/api/signout' @click.native="logout">Logout</router-link>
+                
+                <button v-if="$store.state.loggedIn" @click="logoutUser" class="btn btn-danger">Logout</button>
 
+            </div>
+        </div>
+    </div>`,
+    data: function(){
+        return {
+            loggedIn: localStorage.getItem('auth_token')
+        }
+    },
+    methods:{
+        logoutUser(){
+            localStorage.clear()
+            this.$router.push('/home')
+            this.$emit('logout')
+        }
+    }
 
-    </div>
-    `
 }
